81ec8c0b6bfc1e346e1a7893c70c69699610bfc9
whitenmr <whitenmr@wfu.edu>
2018-04-10 14:24:55 -0400

    database working

--- routes/survey.js
+++ routes/survey.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var MongoCLient = require('mongodb').MongoCLient;
+var MongoClient = require('mongodb').MongoClient;
 
 
 var surveyData = require('../models/surveyData');
 
 var db;
-const url = 'mongodb://​ben​:​admin​@ds237979.mlab.com:37979/ndadocument';
 
-MongoCLient.connect(url, function(err, client){
+const url = 'mongodb://makenzie:admin@ds237979.mlab.com:37979/ndadocument';
+
+MongoClient.connect(url, function(err, client){
 	if (err) return console.log(err)
   	db = client.db('ndadocument') // whatever your database name is
 })
@@ -28,7 +29,8 @@ function record_data(req, res, next) {
 		console.log('inside collection function')
     	if (err) return console.log(err)
     	console.log('saved to database')
-    	res.redirect('/users/doc/docpage')
+
+    	res.redirect('/doc/docpage')
   	})
 }
 //FUCK OFF
