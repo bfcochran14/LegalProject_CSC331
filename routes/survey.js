0f30b8d1dc3275dad9a2ac193d93e73716f24689
Ben Cochran <BenCochran@Bens-MacBook-Pro.local>
2018-04-10 14:48:46 -0400

    simple db working

--- routes/survey.js
+++ routes/survey.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var MongoCLient = require('mongodb').MongoCLient;
+var MongoClient = require('mongodb').MongoClient;
 
 
 var surveyData = require('../models/surveyData');
 
 var db;
-const url = 'mongodb://​ben​:​admin​@ds237979.mlab.com:37979/ndadocument';
+const url = 'mongodb://ben:admin@ds237979.mlab.com:37979/ndadocument';
 
-MongoCLient.connect(url, function(err, client){
+MongoClient.connect(url, function(err, client){
 	if (err) return console.log(err)
   	db = client.db('ndadocument') // whatever your database name is
 })
@@ -28,7 +28,7 @@ function record_data(req, res, next) {
 		console.log('inside collection function')
     	if (err) return console.log(err)
     	console.log('saved to database')
-    	res.redirect('/users/doc/docpage')
+    	res.redirect('/doc/docpage')
   	})
 }
 //FUCK OFF
